1f6ba608004f2bbb9a296a97859fad0f204b33a4
Conor.McCarthy <Conor@Habyts.com>
2016-02-16 17:34:37 +0000

    Update sample

--- app/src/main/java/com/habyts/testandroidnotifications/MainActivity.java
+++ app/src/main/java/com/habyts/testandroidnotifications/MainActivity.java
@@ -1,5 +1,13 @@
 package com.habyts.testandroidnotifications;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import com.google.android.gms.gcm.*;
+import com.microsoft.windowsazure.messaging.*;
+import com.microsoft.windowsazure.notifications.NotificationsManager;
+import android.widget.Toast;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +15,79 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String SENDER_ID = "216337387977";
+    private GoogleCloudMessaging gcm;
+    private NotificationHub hub;
+    private String HubName = "habytsnonprodpushalertnotificationhub";
+    private String HubListenConnectionString = "Endpoint=sb://habytsnonprodnotificationhubns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=7YDQMTV9SKJzQW3TiH6iH6ZrJQOgtiw8X74+ccL4ybM=";
+    private static Boolean isVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyHandler.mainActivity = this;
+        NotificationsManager.handleNotifications(this, SENDER_ID, MyHandler.class);
+        gcm = GoogleCloudMessaging.getInstance(this);
+        hub = new NotificationHub(HubName, HubListenConnectionString, this);
+        registerWithNotificationHubs();
+
+    }
+
+
+    @SuppressWarnings("unchecked")
+    private void registerWithNotificationHubs() {
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object... params) {
+                try {
+                    String regid = gcm.register(SENDER_ID);
+                    ToastNotify("Registered Successfully - RegId : " +
+                            hub.register(regid).getRegistrationId());
+                } catch (Exception e) {
+                    ToastNotify("Registration Exception Message - " + e.getMessage());
+                    return e;
+                }
+                return null;
+            }
+        }.execute(null, null, null);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isVisible = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isVisible = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isVisible = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isVisible = false;
+    }
+
+    public void ToastNotify(final String notificationMessage)
+    {
+        if (isVisible == true)
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, notificationMessage, Toast.LENGTH_LONG).show();
+                }
+            });
     }
 
     @Override
