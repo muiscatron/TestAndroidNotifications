cd564f852dc5cf37452abab1999bab2496f7c7a4
Conor.McCarthy <Conor@Habyts.com>
2016-02-17 11:21:43 +0000

    Add functionality to register app to receive notifications

--- app/src/main/java/com/habyts/testandroidnotifications/MainActivity.java
+++ app/src/main/java/com/habyts/testandroidnotifications/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.AsyncTask;
 import com.google.android.gms.gcm.*;
 import com.microsoft.windowsazure.messaging.*;
 import com.microsoft.windowsazure.notifications.NotificationsManager;
+
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +17,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.Button;
+import java.io.UnsupportedEncodingException;
+import android.content.Context;
+import java.util.HashSet;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import java.io.IOException;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     private String SENDER_ID = "216337387977";
     private GoogleCloudMessaging gcm;
-    private NotificationHub hub;
+    //private NotificationHub hub;
+
+    private RegisterClient registerClient;
+    private static final String BACKEND_ENDPOINT = "http://habyts-test-notifications.azurewebsites.net";
+
     private String HubName = "habytsnonprodpushalertnotificationhub";
     private String HubListenConnectionString = "Endpoint=sb://habytsnonprodnotificationhubns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=7YDQMTV9SKJzQW3TiH6iH6ZrJQOgtiw8X74+ccL4ybM=";
     private static Boolean isVisible = false;
@@ -25,33 +48,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         MyHandler.mainActivity = this;
         NotificationsManager.handleNotifications(this, SENDER_ID, MyHandler.class);
         gcm = GoogleCloudMessaging.getInstance(this);
-        hub = new NotificationHub(HubName, HubListenConnectionString, this);
-        registerWithNotificationHubs();
+        // hub = new NotificationHub(HubName, HubListenConnectionString, this);
+        // registerWithNotificationHubs();
 
-    }
+        registerClient = new RegisterClient(this, BACKEND_ENDPOINT);
 
+        setContentView(R.layout.activity_main);
 
-    @SuppressWarnings("unchecked")
-    private void registerWithNotificationHubs() {
-        new AsyncTask() {
-            @Override
-            protected Object doInBackground(Object... params) {
-                try {
-                    String regid = gcm.register(SENDER_ID);
-                    ToastNotify("Registered Successfully - RegId : " +
-                            hub.register(regid).getRegistrationId());
-                } catch (Exception e) {
-                    ToastNotify("Registration Exception Message - " + e.getMessage());
-                    return e;
-                }
-                return null;
-            }
-        }.execute(null, null, null);
     }
 
 
@@ -59,7 +66,90 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         isVisible = true;
+        Button sendPush = (Button) findViewById(R.id.sendbutton);
+        sendPush.setEnabled(false);
     }
+
+    public void login(View view) throws UnsupportedEncodingException {
+        this.registerClient.setAuthorizationHeader(getAuthorizationHeader());
+
+        final Context context = this;
+        new AsyncTask<Object, Object, Object>() {
+            @Override
+            protected Object doInBackground(Object... params) {
+                try {
+                    String regid = gcm.register(SENDER_ID);
+                    registerClient.register(regid, new HashSet<String>());
+                } catch (Exception e) {
+                    ToastNotify("MainActivity - Failed to register : " + e.getMessage());
+                    return e;
+                }
+                return null;
+            }
+
+            protected void onPostExecute(Object result) {
+                Button sendPush = (Button) findViewById(R.id.sendbutton);
+                sendPush.setEnabled(true);
+                sendPush.setClickable(true);
+                Toast.makeText(context, "Logged in and registered.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }.execute(null, null, null);
+    }
+
+    private String getAuthorizationHeader() throws UnsupportedEncodingException {
+        EditText username = (EditText) findViewById(R.id.usernameText);
+        EditText password = (EditText) findViewById(R.id.passwordText);
+        String basicAuthHeader = username.getText().toString()+":"+password.getText().toString();
+        basicAuthHeader = Base64.encodeToString(basicAuthHeader.getBytes("UTF-8"), Base64.NO_WRAP);
+        return basicAuthHeader;
+    }
+
+
+    /**
+     * This method calls the ASP.NET WebAPI backend to send the notification message
+     * to the platform notification service based on the pns parameter.
+     *
+     * @param pns     The platform notification service to send the notification message to. Must
+     *                be one of the following ("wns", "gcm", "apns").
+     * @param userTag The tag for the user who will receive the notification message. This string
+     *                must not contain spaces or special characters.
+     * @param message The notification message string. This string must include the double quotes
+     *                to be used as JSON content.
+     */
+    public void sendPush(final String pns, final String userTag, final String message)
+            throws ClientProtocolException, IOException {
+        new AsyncTask<Object, Object, Object>() {
+            @Override
+            protected Object doInBackground(Object... params) {
+                try {
+
+                    String uri = BACKEND_ENDPOINT + "/api/notifications";
+                    uri += "?pns=" + pns;
+                    uri += "&to_tag=" + userTag;
+
+                    HttpPost request = new HttpPost(uri);
+                    request.addHeader("Authorization", "Basic "+ getAuthorizationHeader());
+                    request.setEntity(new StringEntity(message));
+                    request.addHeader("Content-Type", "application/json");
+
+                    HttpResponse response = new DefaultHttpClient().execute(request);
+
+                    if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                        ToastNotify("MainActivity - Error sending " + pns + " notification : " +
+                                response.getStatusLine().toString());
+                        throw new RuntimeException("Error sending notification");
+                    }
+                } catch (Exception e) {
+                    ToastNotify("MainActivity - Failed to send " + pns + " notification : " + e.getMessage());
+                    return e;
+                }
+
+                return null;
+            }
+        }.execute(null, null, null);
+    }
+
 
     @Override
     protected void onPause() {
@@ -77,6 +167,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         isVisible = false;
+    }
+
+
+    /**
+     * Send Notification button click handler. This method sends the push notification
+     * message to each platform selected.
+     *
+     * @param v The view
+     */
+    public void sendNotificationButtonOnClick(View v)
+            throws ClientProtocolException, IOException {
+
+        String nhMessageTag = ((EditText) findViewById(R.id.editTextNotificationMessageTag))
+                .getText().toString();
+        String nhMessage = ((EditText) findViewById(R.id.editTextNotificationMessage))
+                .getText().toString();
+
+        // JSON String
+        nhMessage = "\"" + nhMessage + "\"";
+
+        if (((ToggleButton)findViewById(R.id.toggleButtonWNS)).isChecked())
+        {
+            sendPush("wns", nhMessageTag, nhMessage);
+        }
+        if (((ToggleButton)findViewById(R.id.toggleButtonGCM)).isChecked())
+        {
+            sendPush("gcm", nhMessageTag, nhMessage);
+        }
+        if (((ToggleButton)findViewById(R.id.toggleButtonAPNS)).isChecked())
+        {
+            sendPush("apns", nhMessageTag, nhMessage);
+        }
     }
 
     public void ToastNotify(final String notificationMessage)
